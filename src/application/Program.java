c45d24780ee9d6bb55d9174a63a18b3f2421d846
Gabriel Wodtke <gabrielwodtke28@gmail.com>
2025-10-01 22:34:30 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +11,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Department obj = new Department(1,"Books");
-        Seller seller = new  Seller(21, "Bob", "bob@gmail.com", new Date(), obj, 3000.0);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
         }
     }
